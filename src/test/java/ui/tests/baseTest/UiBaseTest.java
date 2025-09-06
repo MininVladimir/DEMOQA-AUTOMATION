168476cf1932f74b921d48fe9b44118c9a524e8d
MininVladimir <114000898+MininVladimir@users.noreply.github.com>
2025-09-06 22:15:18 +0300

    feat(CORE_01): implement allure screenshot listener (#2)

--- src/test/java/ui/tests/baseTest/UiBaseTest.java
+++ src/test/java/ui/tests/baseTest/UiBaseTest.java
@@ -1,17 +1,20 @@
 package ui.tests.baseTest;
 
+import core.listener.screenshotListener.IScreenshotListener;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
-public class UiBaseTest implements IUiBaseTest {
+public class UiBaseTest implements IUiBaseTest, IScreenshotListener {
 
   @BeforeAll
   public static void setup() throws Exception {
     IUiBaseTest.setupDriver();
+    IScreenshotListener.addScreenshotListener();
   }
 
   @AfterAll
   public static void tearDown() {
+    IScreenshotListener.removeScreenshotListener();
     IUiBaseTest.closeDriver();
   }
 }
