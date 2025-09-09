16b35e03086b61862765e1829aa7b4b23fa6f704
MininVladimir <minin.vladimir.90@gmail.com>
2025-09-09 05:34:25 +0300

    docs(project): add PA_UI_06 scenario

--- src/test/java/docs/tetsDescriptions/TestDescriptions.java
+++ src/test/java/docs/tetsDescriptions/TestDescriptions.java
@@ -6,4 +6,5 @@ public class TestDescriptions {
   public static final String PA_UI_03 = "PA_UI_03 - Login new user with using cookies";
   public static final String REG_UI_04 = "REG_UI_04 - Registration user";
   public static final String PA_UI_05 = "PA_UI_05 - Login new user";
+  public static final String PA_UI_06 = "PA_UI_06 - Logout";
 }
