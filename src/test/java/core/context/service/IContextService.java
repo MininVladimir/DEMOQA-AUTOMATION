286b6db194ef1a8421c619a323a021f6b8cae582
MininVladimir <minin.vladimir.90@gmail.com>
2025-09-11 20:31:29 +0300

    refactor(project): refactor ICOntextService

--- src/test/java/core/context/service/IContextService.java
+++ src/test/java/core/context/service/IContextService.java
@@ -1,88 +1,92 @@
 package core.context.service;
 
+import core.context.api.ApiContext;
 import core.context.api.ApiContextHolder;
+import core.context.ui.UiContext;
 import core.context.ui.UiContextHolder;
 
 import static core.generator.Generator.stringGenerator;
 
 public interface IContextService {
+  UiContext uiContext = UiContextHolder.getContext();
+  ApiContext apiCOntext = ApiContextHolder.getContext();
 
   static void setFirstNameToContext() {
-    UiContextHolder.getContext().setFirstname(stringGenerator(6, 0, 0, 0));
+    uiContext.setFirstname(stringGenerator(6, 0, 0, 0));
   }
 
   static void setLastNameToContext() {
-    UiContextHolder.getContext().setLastname(stringGenerator(6, 0, 0, 0));
+    uiContext.setLastname(stringGenerator(6, 0, 0, 0));
   }
 
   static void setUserUsernameToContext(String contextType) {
     switch (contextType) {
-      case "api" -> ApiContextHolder.getContext().setUsername(stringGenerator(6, 0, 0, 0));
-      case "ui" -> UiContextHolder.getContext().setUsername(stringGenerator(6, 0, 0, 0));
+      case "api" -> apiCOntext.setUsername(stringGenerator(6, 0, 0, 0));
+      case "ui" -> uiContext.setUsername(stringGenerator(6, 0, 0, 0));
       default -> throw new IllegalStateException("Unexpected value: " + contextType);
     }
   }
 
   static void setPasswordToContext(String contextType) {
     switch (contextType) {
-      case "api" -> ApiContextHolder.getContext().setPassword(stringGenerator(3, 3, 3, 3));
-      case "ui" -> UiContextHolder.getContext().setPassword(stringGenerator(3, 3, 3, 3));
+      case "api" -> apiCOntext.setPassword(stringGenerator(3, 3, 3, 3));
+      case "ui" -> uiContext.setPassword(stringGenerator(3, 3, 3, 3));
       default -> throw new IllegalStateException("Unexpected value: " + contextType);
     }
   }
 
   static void setUserIdToContext(String contextType, String userId) {
     switch (contextType) {
-      case "api" -> ApiContextHolder.getContext().setUserId(userId);
-      case "ui" -> UiContextHolder.getContext().setUserId(userId);
+      case "api" -> apiCOntext.setUserId(userId);
+      case "ui" -> uiContext.setUserId(userId);
       default -> throw new IllegalStateException("Unexpected value: " + contextType);
     }
   }
 
   static String getUserIdFromContext(String contextType) {
     return switch (contextType) {
-      case "api" -> ApiContextHolder.getContext().getUserId();
-      case "ui" -> UiContextHolder.getContext().getUserId();
+      case "api" -> apiCOntext.getUserId();
+      case "ui" -> uiContext.getUserId();
       default -> throw new IllegalStateException("Unexpected value: " + contextType);
     };
   }
 
   static String getUsernameFromContext(String contextType) {
     return switch (contextType) {
-      case "api" -> ApiContextHolder.getContext().getUsername();
-      case "ui" -> UiContextHolder.getContext().getUsername();
+      case "api" -> apiCOntext.getUsername();
+      case "ui" -> uiContext.getUsername();
       default -> throw new IllegalStateException("Unexpected value: " + contextType);
     };
   }
 
   static String getPasswordFromContext(String contextType) {
     return switch (contextType) {
-      case "api" -> ApiContextHolder.getContext().getPassword();
-      case "ui" -> UiContextHolder.getContext().getPassword();
+      case "api" -> apiCOntext.getPassword();
+      case "ui" -> uiContext.getPassword();
       default -> throw new IllegalStateException("Unexpected value: " + contextType);
     };
   }
 
   static void setTokenToContext(String contextType, String token) {
     switch (contextType) {
-      case "api" -> ApiContextHolder.getContext().setToken(token);
-      case "ui" -> UiContextHolder.getContext().setToken(token);
+      case "api" -> apiCOntext.setToken(token);
+      case "ui" -> uiContext.setToken(token);
       default -> throw new IllegalStateException("Unexpected value: " + contextType);
     }
   }
 
   static String getTokenFromContext(String contextType) {
     return switch (contextType) {
-      case "api" -> ApiContextHolder.getContext().getToken();
-      case "ui" -> UiContextHolder.getContext().getToken();
+      case "api" -> apiCOntext.getToken();
+      case "ui" -> uiContext.getToken();
       default -> throw new IllegalStateException("Unexpected value: " + contextType);
     };
   }
 
   static void setExpiresDateToContext(String contextType, String expiresDate) {
     switch (contextType) {
-      case "api" -> ApiContextHolder.getContext().setExpiresDate(expiresDate);
-      case "ui" -> UiContextHolder.getContext().setExpiresDate(expiresDate);
+      case "api" -> apiCOntext.setExpiresDate(expiresDate);
+      case "ui" -> uiContext.setExpiresDate(expiresDate);
       default -> throw new IllegalStateException("Unexpected value: " + contextType);
     }
   }
